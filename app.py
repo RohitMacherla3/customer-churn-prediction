ecdb90f9406c68c1aa9f977a929a11e1bebf1852
Rohit Macherla <rm1667@scarletmail.rutgers.edu>
2024-04-04 20:58:19 -0400

    Feature scaling and UI updates

--- app.py
+++ app.py
@@ -5,20 +5,43 @@ import numpy as np
 
 st.title('Customer Churn Prediction')
 
+st.write('Check all that apply to the customer')
 
-Dependents = st.selectbox('DO you have Dependents?', ['Yes', 'No'])
-Tenure_Months = int(st.number_input('Tenure'))
-Online_Security = st.selectbox('Online Security', ['Yes', 'No'])
-Online_Backup = st.selectbox('Online Backup', ['Yes', 'No'])
-Device_Protection = st.selectbox('Device Protection', ['Yes', 'No'])
-Tech_Support = st.selectbox('Tech Support', ['Yes', 'No'])
+col1, col2, col3 = st.columns(3)
+Dependents = col1.checkbox('Dependents')
+Online_Security = col2.checkbox('Online Security')
+Online_Backup = col3.checkbox('Online Backup')
+
+col4, col5, col6 = st.columns(3)
+Tech_Support = col4.checkbox('Tech Support')
+Device_Protection = col5.checkbox('Device Protection')
+Internet_Service_No = col6.checkbox('Internet Service')
+
+col7, col8 = st.columns(2)
+Payment_Method_Electronic_check = col7.checkbox('Payment method is electronic check')
+Internet_Service_Fiber_optic = col8.checkbox('Internet Service is fiber optics')
+
+col9, col10 = st.columns(2)
+Monthly_Charges = col9.number_input('Monthly Charges')
+Total_Charges = col10.number_input('Total Charges')
+
 Contract_Two_year = st.selectbox('Contract', ['Month-to-month', 'One year', 'Two year'])
-Internet_Service_No = st.selectbox('Do you have Internet Service?', ['Yes', 'No'])
-Internet_Service_Fiber_optic = st.selectbox('Is your Internet Service fiber optics', ['Yes', 'No'])
-Monthly_Charges = st.number_input('Monthly Charges')
-Total_Charges = st.number_input('Total Charges')
-Payment_Method_Electronic_check = st.selectbox('Is your payment method through electronic check?', ['Yes', 'No'])
+Tenure_Months = st.number_input('Tenure')
 
+st.markdown(
+        """
+        <style>
+        /* Increase the size of the button */
+        .stButton>button {
+            width: 200px; 
+            height: 50px; 
+            margin: 0 auto;
+            display: block;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 if st.button('Predict Churn'):
     model = pickle.load(open('churn_xgb_optimal.pkl', 'rb'))
     data = [[Dependents, Tenure_Months, Online_Security, Online_Backup, Device_Protection, Tech_Support, 
@@ -34,17 +57,17 @@ if st.button('Predict Churn'):
     
 
     for feature in categorical_feature:
-        df[feature] = df[feature].apply(lambda x: 0 if x == "No" else 1)
+        df[feature] = df[feature].apply(lambda x: 1 if x else 0)
     
     df['Contract_Two_year'] = df['Contract_Two_year'].apply(lambda x: 1 if x == 'Two year' else 0)
-    df['Internet_Service_No'] = df['Internet_Service_No'].apply(lambda x: 1 if x == 'No' else 0)
+    df['Internet_Service_No'] = df['Internet_Service_No'].apply(lambda x: 0 if x else 1)
 
     single = model.predict(df)
     probability = model.predict_proba(df)[0][single]
     probability = np.round(probability[0]*100, 2)
 
     if single == 1:
-        op1 = "This Customer is likely to be Churned!"
+        op1 = "This Customer is likely to be Churned :("
         op2 = f"Confidence level is {probability}"
     else:
         op1 = "This Customer is likely to be Continue!"

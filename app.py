317f70a2afb13cf2472775a2482a2846136e4e63
Rohit Macherla <rm1667@scarletmail.rutgers.edu>
2024-04-04 20:12:16 -0400

    Initial running app upload

--- app.py
+++ app.py
@@ -1,45 +1,54 @@
 import streamlit as st
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 import pickle
 import numpy as np
 
 st.title('Customer Churn Prediction')
 
 
-Dependents = st.selectbox('Dependents', ['Yes', 'No'])
-tenure = st.number_input('tenure')
-OnlineSecurity = st.selectbox('OnlineSecurity', ['Yes', 'No'])
-OnlineBackup = st.selectbox('OnlineBackup', ['Yes', 'No'])
-DeviceProtection = st.selectbox('DeviceProtection', ['Yes', 'No'])
-TechSupport = st.selectbox('TechSupport', ['Yes', 'No'])
-Contract = st.selectbox('Contract', ['Month-to-month', 'One year', 'Two year'])
-PaperlessBilling = st.selectbox('PaperlessBilling', ['Yes', 'No'])
-MonthlyCharges = st.number_input('MonthlyCharges')
-TotalCharges = st.number_input('TotalCharges')
+Dependents = st.selectbox('DO you have Dependents?', ['Yes', 'No'])
+Tenure_Months = int(st.number_input('Tenure'))
+Online_Security = st.selectbox('Online Security', ['Yes', 'No'])
+Online_Backup = st.selectbox('Online Backup', ['Yes', 'No'])
+Device_Protection = st.selectbox('Device Protection', ['Yes', 'No'])
+Tech_Support = st.selectbox('Tech Support', ['Yes', 'No'])
+Contract_Two_year = st.selectbox('Contract', ['Month-to-month', 'One year', 'Two year'])
+Internet_Service_No = st.selectbox('Do you have Internet Service?', ['Yes', 'No'])
+Internet_Service_Fiber_optic = st.selectbox('Is your Internet Service fiber optics', ['Yes', 'No'])
+Monthly_Charges = st.number_input('Monthly Charges')
+Total_Charges = st.number_input('Total Charges')
+Payment_Method_Electronic_check = st.selectbox('Is your payment method through electronic check?', ['Yes', 'No'])
 
-model = pickle.load(open('churn_xgb_optimal.pkl', 'rb'))
-data = [[Dependents, tenure, OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport, Contract, PaperlessBilling, MonthlyCharges, TotalCharges]]
-df = pd.DataFrame(data, columns=['Dependents', 'tenure', 'OnlineSecurity',
-    'OnlineBackup', 'DeviceProtection', 'TechSupport', 'Contract',
-    'PaperlessBilling', 'MonthlyCharges', 'TotalCharges'])
+if st.button('Predict Churn'):
+    model = pickle.load(open('churn_xgb_optimal.pkl', 'rb'))
+    data = [[Dependents, Tenure_Months, Online_Security, Online_Backup, Device_Protection, Tech_Support, 
+             Monthly_Charges, Total_Charges, Internet_Service_Fiber_optic, Internet_Service_No, Contract_Two_year, 
+             Payment_Method_Electronic_check]]
+    df = pd.DataFrame(data, columns=['Dependents', 'Tenure_Months', 'Online_Security', 'Online_Backup',
+                                     'Device_Protection', 'Tech_Support', 'Monthly_Charges', 'Total_Charges',
+                                     'Internet_Service_Fiber_optic', 'Internet_Service_No', 'Contract_Two_year', 
+                                     'Payment_Method_Electronic_check'])
 
-categorical_feature = {feature for feature in df.columns if df[feature].dtypes == 'O'}
+    categorical_feature = ['Dependents', 'Online_Security', 'Online_Backup', 'Device_Protection', 'Tech_Support',
+                           'Internet_Service_Fiber_optic', 'Payment_Method_Electronic_check']
+    
 
-encoder = LabelEncoder()
-for feature in categorical_feature:
-    df[feature] = encoder.fit_transform(df[feature])
+    for feature in categorical_feature:
+        df[feature] = df[feature].apply(lambda x: 0 if x == "No" else 1)
+    
+    df['Contract_Two_year'] = df['Contract_Two_year'].apply(lambda x: 1 if x == 'Two year' else 0)
+    df['Internet_Service_No'] = df['Internet_Service_No'].apply(lambda x: 1 if x == 'No' else 0)
 
-single = model.predict(df)
-probability = model.predict_proba(df)[:, 1]
-probability = probability*100
+    single = model.predict(df)
+    probability = model.predict_proba(df)[0][single]
+    probability = np.round(probability[0]*100, 2)
 
-if single == 1:
-    op1 = "This Customer is likely to be Churned!"
-    op2 = f"Confidence level is {np.round(probability[0], 2)}"
-else:
-    op1 = "This Customer is likely to be Continue!"
-    op2 = f"Confidence level is {np.round(probability[0], 2)}"
+    if single == 1:
+        op1 = "This Customer is likely to be Churned!"
+        op2 = f"Confidence level is {probability}"
+    else:
+        op1 = "This Customer is likely to be Continue!"
+        op2 = f"Confidence level is {probability}"
 
-st.write(op1)
-st.write(op2)
+    st.write(op1)
+    st.write(op2)
